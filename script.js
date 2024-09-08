59420cd7c75faf6bee906fb70642983cff380224
Junaid Ul Haque sheikh <156045075+junaidsheikh0002@users.noreply.github.com>
2024-09-08 12:34:59 +0500

    https://github.com/junaidsheikh0002/Dynamic-Resume-Builder-By-Junaid-Ul-Haque-Sheikh/upload
    https://github.com/junaidsheikh0002/Dynamic-Resume-Builder-By-Junaid-Ul-Haque-Sheikh/upload
    https://github.com/junaidsheikh0002/Dynamic-Resume-Builder-By-Junaid-Ul-Haque-Sheikh/upload
    https://github.com/junaidsheikh0002/Dynamic-Resume-Builder-By-Junaid-Ul-Haque-Sheikh/upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function generateResume() {
+    document.getElementById("outputName").textContent = document.getElementById("name").value;
+    document.getElementById("outputEmail").textContent = document.getElementById("email").value;
+    document.getElementById("outputPhone").textContent = document.getElementById("phone").value;
+    document.getElementById("outputEducation").textContent = document.getElementById("education").value;
+    document.getElementById("outputExperience").textContent = document.getElementById("experience").value;
+    document.getElementById("outputSkills").textContent = document.getElementById("skills").value;
+}
